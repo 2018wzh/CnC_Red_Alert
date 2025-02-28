5203473a444ae8153ae50023b6e63c88f7383070
James Urquhart <jamesu@gmail.com>
2025-02-28 20:07:03 +0000

    Correct launch script

--- launch_dosbox.sh
+++ launch_dosbox.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ROOT_FOLDER=`pwd`
 # Note: openwatcom should be placed in watcom/
-dosbox-x -c "MOUNT C $ROOT_FOLDER/openwatcom" -c "MOUNT D $ROOT_FOLDER"
+dosbox-x -c "MOUNT C $ROOT_FOLDER/watcom" -c "MOUNT D $ROOT_FOLDER" -c "mode con lines=60" -c "mode con cols=132"

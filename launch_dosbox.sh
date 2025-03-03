afcd34c75fc04a7fa7421d01ff5e3b92502844f8
James Urquhart <jamesu@gmail.com>
2025-03-03 01:12:33 +0000

    Update env

--- launch_dosbox.sh
+++ launch_dosbox.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ROOT_FOLDER=`pwd`
 # Note: openwatcom should be placed in watcom/
-dosbox-x -c "MOUNT C $ROOT_FOLDER/watcom" -c "MOUNT D $ROOT_FOLDER" -c "mode con lines=60" -c "mode con cols=132"
+dosbox-x -set "dos xms=true" -set "dos xmslimit=64" -set "dos emslimit=64" -c "MOUNT C $ROOT_FOLDER/watcom" -c "MOUNT D $ROOT_FOLDER" -c "mode con lines=60" -c "mode con cols=132"
